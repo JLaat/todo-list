113ca13500048627bca1affde310be18d2406ef3
Joni Laatikainen <jonilaatikaine@gmail.com>
2022-04-15 11:08:35 +0300

    Started creating game controller

--- src/projects.js
+++ src/projects.js
@@ -1,8 +1,14 @@
 export {project};
 
+let projectList = [];
+
 const project = (projectName) => {
     let newProject = {
         projectName: projectName,
     }
     return newProject;
+}
+
+const removeProject = (project) => {
+    projectList.splice(projectList.indexOf(project), 1);
 }
